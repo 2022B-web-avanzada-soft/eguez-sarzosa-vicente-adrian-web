6ac8d3303c363e6388b26465c532ce7e665d6dd0
Adrian Eguez <eadepto@hotmail.com>
2022-12-15 17:57:01 -0500

    Auto stash before merge of "main" and "origin/main"

--- 01-javascript/08-promesas.js
+++ 01-javascript/08-promesas.js
@@ -1,5 +1,6 @@
 // 08-promesas.js
 const fs = require('fs');
+
 /*
 * Una funcion que acepte como parametro una variable
 * del "path" del archivo y otra variable con el "contenidoArchivo".
@@ -7,17 +8,87 @@ const fs = require('fs');
 * "contenidoArchivo" a ese archivo.
 * */
 
-function leerArchivo(path){
-    return new Promise(); //
+function leerArchivo(path) {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(
+                path, // Nombre o path del archivo
+                'utf-8', // codificacion
+                (errorLectura, contenido) => {
+                    if (errorLectura) {
+                        console.error(errorLectura);
+                        reject('Error leyendo primer archivo');
+                    } else {
+                        resolve(contenido)
+                    }
+                }
+            );
+        }
+    ); //
 }
-function escribirArchivo(path){
-    return new Promise(); //
+
+function escribirArchivo(path, nuevoContenido) {
+    return new Promise(
+        (resolve, reject) => {
+            fs.writeFile(
+                path,
+                nuevoContenido,
+                (errorEscritura) => {
+                    if (errorEscritura) {
+                        console.error(errorEscritura);
+                        reject('Error escribiendo nuevo archivo');
+                    } else {
+                        resolve('Completado');
+                    }
+                }
+            );
+        }
+    ); //
 }
+
 //
-function ejercicio08(path, contenidoArchivo){
-
+function ejercicio08(path, contenidoNuevo) {
+    leerArchivo(path)
+        .then(
+            (contenidoArchivoOriginal) => {
+                return escribirArchivo(
+                    path, contenidoArchivoOriginal + contenidoNuevo
+                )
+            }
+        )
+        .then(
+            (mensajeCreado) => {
+                console.log(mensajeCreado);
+            }
+        )
+        .catch((error) => console.error(error));
 }
 
-ejercicio08('06-ejemplo.txt', ' :)  lo logramos!')
-.then()
-.catch()
+// ejercicio08('06-ejemplo.txt', ' :)  lo logramos!');
+
+// ASYNC AWAIT
+// REGLAS:
+// 1) Estar dentro de una funcion (nombrada o anonima)
+// 2) AGREGAR la palabra 'async' antes de la declaracion
+//    de la funcion
+// 3) AGREGAR la palabra 'await' antes de la declaracion
+//    de una promesa
+async function asyncAwaitUno(path, nuevoContenido) {
+    // Si sabemos que en la promesa PUEDE haber un reject, usamos try y catch
+    try {
+        const respuestaContenidoArchivoOriginal = await leerArchivo(path);
+        await escribirArchivo(path, respuestaContenidoArchivoOriginal + nuevoContenido );
+        1 + 1; // caramelo
+        leerArchivo().then().catch() // async
+        await leerArchivo() // sync
+        // await escribirArchivo(path, (await leerArchivo(path)) + nuevoContenido );
+    } catch (error) {
+        console.error(error);
+    }
+}
+asyncAwaitUno('06-ejemplo.txt', ' :)  lo logramos!').then().catch()
+const asyncAwaitDos = async function () {
+}
+const asyncAwaitTres = async () => {
+}
+
---
+++ 01-javascript/proyecto-dos/09-inquirer.js
@@ -0,0 +1,4 @@
+// 09-inquirer.js
+// npm init -> package.json -> dependencias -> scripts
+// npm install inquirer -> npm i inquirer
+// node_modules -> estan las dependencias
