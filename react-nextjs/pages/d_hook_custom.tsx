d6d9927a1f58394682c04206dd3e3a31a54261aa
Adrian Eguez <eadepto@hotmail.com>
2023-01-10 08:54:29 -0500

    hasta custom hooks

---
+++ react-nextjs/pages/d_hook_custom.tsx
@@ -0,0 +1,25 @@
+// pages/d_hook_custom.tsx
+import HookCustom from "../components/d_hook_custom/HookCustom";
+
+export default function (){
+    return (
+        <>
+            <h1>Hook Custom</h1>
+            <HookCustom></HookCustom>
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
